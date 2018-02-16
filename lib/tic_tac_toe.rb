8f9a7a09224d69c3c7ca6717a558ab2367c4d214
Vincent Huang <vincent.huang@wework.com>
2018-02-16 00:05:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,6 +51,10 @@ class TicTacToe
     end
   end
 
+  def current_player()
+    turn_count(@board) % 2 == 0 ? "X" : "O"
+  end
+
   def turn_count()
     @board.count{|token| token == "X" || token == "O"}
   end
